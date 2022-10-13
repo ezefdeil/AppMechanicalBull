b523acc782bee291c5e51d6e5adbc5eb2df0bcc8
Ezequiel Fdeil <ezequiel.fdeil@gmail.com>
2022-10-13 00:46:44 -0300

    Add project files.

---
+++ AppMechanicalBull/AppMechanicalBull/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace AppMechanicalBull;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
